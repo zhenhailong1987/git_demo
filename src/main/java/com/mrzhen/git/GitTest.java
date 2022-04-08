266e955319dc37fef34df7c636acd866a0115803
mrzhen <327372897@qq.com>
2022-04-08 18:23:06 +0800

    commit555 by master

--- src/main/java/com/mrzhen/git/GitTest.java
+++ src/main/java/com/mrzhen/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git222");
         System.out.println("hello git333");
         System.out.println("hello git444");
+        System.out.println("hello git555");
 
     }
 }
